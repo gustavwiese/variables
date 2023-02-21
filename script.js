5822ad7c69d83f2b3419e341422eec8caa4912d6
gustavwiese <gusse12345@gmail.com>
2023-02-21 13:15:45 +0100

    play with variables

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log("script.js is running");
+
+let name = "Gustav Duer Wiese Pedersen";
+
+let initials = "gupe0001";
+
+let age = "24 years old";
+
+let city = "KøbenhavnS";
+
+console.log(name);
+console.log(age);
+console.log(initials);
+console.log(age);
+console.log(city);
+
+document.querySelector("#full-name").textContent = name;
+document.querySelector("#initials").textContent = initials;
+document.querySelector("#age").textContent = age;
+document.querySelector("#city").textContent = city;
